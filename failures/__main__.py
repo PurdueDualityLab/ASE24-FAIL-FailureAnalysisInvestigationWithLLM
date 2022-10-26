ccd760be1fa94817eef1121d3890946615213a76
Diego Montes <d57montes@hotmail.com>
2022-10-26 01:37:41 -0400

    add django setup in main

--- failures/__main__.py
+++ failures/__main__.py
@@ -1,4 +1,17 @@
-from failures.commands import main
+import os
+import sys
+from pathlib import Path
+
+import django
 
 if __name__ == "__main__":
+    ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+    sys.path.append(str(ROOT_DIR / "failures"))
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+    django.setup()
+
+    from failures.commands import main
+
     main()
