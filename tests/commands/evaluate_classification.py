8e3ba6b5f283dbf6fc0ad3010bb11c39f0f015a0
Matthew Campbell <campb474@gmail.com>
2023-11-13 11:57:10 -0500

    Updaing eval classify

--- tests/commands/evaluate_classification.py
+++ tests/commands/evaluate_classification.py
@@ -113,11 +113,11 @@ class EvaluateClassificationCommand:
                         ground_truth
                     ))
                 # If --all then update false positives and negatives
-                if args.all:
-                    if article.describes_failure != None and article.describes_failure == 1 and ground_truth == 0:
-                        false_positives += 1
-                    elif article.describes_failure != None and article.describes_failure == 0 and ground_truth == 1:
-                        false_negatives += 1
+                # if args.all:
+                if article.describes_failure != None and article.describes_failure == 1 and ground_truth == 0:
+                    false_positives += 1
+                elif article.describes_failure != None and article.describes_failure == 0 and ground_truth == 1:
+                    false_negatives += 1
 
         # Checking to see if there are any matching articles
         if total_articles > 0:
@@ -126,44 +126,44 @@ class EvaluateClassificationCommand:
             logging.info(f"Accuracy: {accuracy_percentage:.2f}% ({total_match}/{total_articles})")
 
             # Checking if --list
-            if args.list:
-                logging.info('List of incorrectly classified articles:')
-                for article in incorrectly_classified_articles:
-                    logging.info(article)
+            # if args.list:
+            #     logging.info('List of incorrectly classified articles:')
+            #     for article in incorrectly_classified_articles:
+            #         logging.info(article[0])
 
             # Checkign if --all
-            if args.all:
-                # Calculate false positive and false negative rates as both fractions and percentages
-                false_positive_rate = (false_positives / total_articles) * 100
-                false_negative_rate = (false_negatives / total_articles) * 100
-                false_positive_fraction = f"{false_positives}/{total_articles}"
-                false_negative_fraction = f"{false_negatives}/{total_articles}"
+            # if args.all:
+            # Calculate false positive and false negative rates as both fractions and percentages
+            false_positive_rate = (false_positives / total_articles) * 100
+            false_negative_rate = (false_negatives / total_articles) * 100
+            false_positive_fraction = f"{false_positives}/{total_articles}"
+            false_negative_fraction = f"{false_negatives}/{total_articles}"
 
-                # Calculate the number and percentage of correct and wrong classifications
-                correct_classifications = total_match
-                wrong_classifications = total_articles - total_match
-                wrong_percentage = (wrong_classifications / total_articles) * 100
+            # Calculate the number and percentage of correct and wrong classifications
+            correct_classifications = total_match
+            wrong_classifications = total_articles - total_match
+            wrong_percentage = (wrong_classifications / total_articles) * 100
 
-                logging.info(f"False Positives: {false_positive_rate:.2f}% ({false_positive_fraction})")
+            logging.info(f"False Positives: {false_positive_rate:.2f}% ({false_positive_fraction})")
 
-                # Print out all false positives
-                if args.list:
-                    logging.info('List of all false positive classifications:')
-                    for article in incorrectly_classified_articles:
-                        if article[1] == 0:
-                            logging.info(article[0])
-                
-                logging.info(f"False Negatives: {false_negative_rate:.2f}% ({false_negative_fraction})")
+            # Print out all false positives
+            if args.list:
+                logging.info('List of all false positive classifications:')
+                for article in incorrectly_classified_articles:
+                    if article[1] == 0:
+                        logging.info(article[0])
+            
+            logging.info(f"False Negatives: {false_negative_rate:.2f}% ({false_negative_fraction})")
 
-                # Print out all false positives
-                if args.list:
-                    logging.info('List of all false negative classifications:')
-                    for article in incorrectly_classified_articles:
-                        if article[1] == 1:
-                            logging.info(article[0])
+            # Print out all false positives
+            if args.list:
+                logging.info('List of all false negative classifications:')
+                for article in incorrectly_classified_articles:
+                    if article[1] == 1:
+                        logging.info(article[0])
 
-                logging.info(f"Wrong: {wrong_percentage:.2f}% ({wrong_classifications}/{total_articles})")
-                logging.info(f"Total Evaluated: {total_articles}")
+            logging.info(f"Wrong: {wrong_percentage:.2f}% ({wrong_classifications}/{total_articles})")
+            logging.info(f"Total Evaluated: {total_articles}")
 
             metrics = {
                 "Classify: Accuracy (Percentage)": f"{accuracy_percentage:.2f}%",
