e6f3e728290d5d07e5f1fe8bbe6d835f8fcee5b5
Matthew Campbell <campb474@gmail.com>
2023-11-21 17:59:08 -0500

    Separating classification command validation

--- tests/commands/evaluate_identification.py
+++ tests/commands/evaluate_identification.py
@@ -111,11 +111,11 @@ class EvaluateIdentificationCommand:
                         ground_truth
                     ))
                 # If --all then update false positives and negatives
-                if args.all:
-                    if article.analyzable_failure != None and article.analyzable_failure == 1 and ground_truth == 0:
-                        false_positives += 1
-                    elif article.analyzable_failure != None and article.analyzable_failure == 0 and ground_truth == 1:
-                        false_negatives += 1
+                # if args.all:
+                if article.analyzable_failure != None and article.analyzable_failure == 1 and ground_truth == 0:
+                    false_positives += 1
+                elif article.analyzable_failure != None and article.analyzable_failure == 0 and ground_truth == 1:
+                    false_negatives += 1
 
         # Checking to see if there are any matching articles
         if total_articles > 0:
@@ -129,50 +129,50 @@ class EvaluateIdentificationCommand:
             #         logging.info(article[0])
 
             # Checkign if --all
-            if args.all:
-                # Calculate false positive and false negative rates as both fractions and percentages
-                false_positive_rate = (false_positives / total_articles) * 100
-                false_negative_rate = (false_negatives / total_articles) * 100
-                false_positive_fraction = f"{false_positives}/{total_articles}"
-                false_negative_fraction = f"{false_negatives}/{total_articles}"
+            # if args.all:
+            # Calculate false positive and false negative rates as both fractions and percentages
+            false_positive_rate = (false_positives / total_articles) * 100
+            false_negative_rate = (false_negatives / total_articles) * 100
+            false_positive_fraction = f"{false_positives}/{total_articles}"
+            false_negative_fraction = f"{false_negatives}/{total_articles}"
 
-                # Calculate the number and percentage of correct and wrong classifications
-                correct_classifications = total_match
-                wrong_classifications = total_articles - total_match
-                wrong_percentage = (wrong_classifications / total_articles) * 100
+            # Calculate the number and percentage of correct and wrong classifications
+            correct_classifications = total_match
+            wrong_classifications = total_articles - total_match
+            wrong_percentage = (wrong_classifications / total_articles) * 100
 
-                logging.info(f"False Positives: {false_positive_rate:.2f}% ({false_positive_fraction})")
+            logging.info(f"False Positives: {false_positive_rate:.2f}% ({false_positive_fraction})")
 
-                # Print out all false positives
-                if args.list:
-                    logging.info('List of all false positive identifications:')
-                    for article in incorrectly_classified_articles:
-                        if article[1] == 0:
-                            logging.info(article[0])
+            # Print out all false positives
+            if args.list:
+                logging.info('List of all false positive identifications:')
+                for article in incorrectly_classified_articles:
+                    if article[1] == 0:
+                        logging.info(article[0])
 
-                logging.info(f"False Negatives: {false_negative_rate:.2f}% ({false_negative_fraction})")
+            logging.info(f"False Negatives: {false_negative_rate:.2f}% ({false_negative_fraction})")
 
-                # Print out all false positives
-                if args.list:
-                    logging.info('List of all false negative identifications:')
-                    for article in incorrectly_classified_articles:
-                        if article[1] == 1:
-                            logging.info(article[0])
+            # Print out all false positives
+            if args.list:
+                logging.info('List of all false negative identifications:')
+                for article in incorrectly_classified_articles:
+                    if article[1] == 1:
+                        logging.info(article[0])
 
-                logging.info(f"Wrong: {wrong_percentage:.2f}% ({wrong_classifications}/{total_articles})")
-                logging.info(f"Total Evaluated: {total_articles}")
+            logging.info(f"Wrong: {wrong_percentage:.2f}% ({wrong_classifications}/{total_articles})")
+            logging.info(f"Total Evaluated: {total_articles}")
 
-                metrics = {
-                    "Identify: Accuracy (Percentage)": f"{accuracy_percentage:.2f}%",
-                    "Identify: Accuracy (Fraction)": f"{total_match}/{total_articles}",
-                    "Identify: False Positive (Percentage)": f"{false_positive_rate:.2f}%",
-                    "Identify: False Positive (Fraction)": f"{false_positive_fraction}",
-                    "Identify: False Negative (Percentage)": f"{false_negative_rate:.2f}%",
-                    "Identify: False Negative (Fraction)": f"{false_negative_fraction}",
-                    "Identify: Wrong (Percentage)": f"{wrong_percentage:.2f}%",
-                    "Identify: Wrong (Fraction)": f"{wrong_classifications}/{total_articles}",
-                    "Identify: Total Evaluated": str(total_articles) 
-                }
+            metrics = {
+                "Identify: Accuracy (Percentage)": f"{accuracy_percentage:.2f}%",
+                "Identify: Accuracy (Fraction)": f"{total_match}/{total_articles}",
+                "Identify: False Positive (Percentage)": f"{false_positive_rate:.2f}%",
+                "Identify: False Positive (Fraction)": f"{false_positive_fraction}",
+                "Identify: False Negative (Percentage)": f"{false_negative_rate:.2f}%",
+                "Identify: False Negative (Fraction)": f"{false_negative_fraction}",
+                "Identify: Wrong (Percentage)": f"{wrong_percentage:.2f}%",
+                "Identify: Wrong (Fraction)": f"{wrong_classifications}/{total_articles}",
+                "Identify: Total Evaluated": str(total_articles) 
+            }
         else:
             logging.info("Evaluate Identification Command: No common IDs found between ground truth and predicted data.")
 
