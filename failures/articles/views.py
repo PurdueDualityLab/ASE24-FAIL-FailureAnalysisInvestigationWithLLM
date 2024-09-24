39f5f2bda4805be29b6c86ec5c455a16dfe1c838
Tanmay Singla <singlat@purdue.edu>
2024-09-24 18:27:11 -0400

    new_ui

--- failures/articles/views.py
+++ failures/articles/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-
-
-
-
+from django.shortcuts import render, get_object_or_404
 from .models import Incident
-
-
 
 def index(request):
     return render(
@@ -15,3 +10,14 @@ def index(request):
             "incidents": Incident.objects.all(),
         },
     )
+
+#user front view
+def public_page(request):
+    incidents = Incident.objects.prefetch_related('articles').all()  # Fetch incidents with related articles
+    return render(request, 'articles/public_page.html', {'incidents': incidents})
+
+# View for individual incident detail page
+def incident_detail_view(request, pk):
+    # Fetch the incident by primary key, or return 404 if not found
+    incident = get_object_or_404(Incident, pk=pk)
+    return render(request, 'articles/incident_detail.html', {'incident': incident})
